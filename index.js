a62b9326484ae8dcf6003e82cfb3e0078fe86115
EtienneDuv <etienne.duverney@viacesi.fr>
2021-01-29 17:34:38 +0100

    [add] SQL output

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const parser = new Parser('mysql');
 
 const {parseSchema} = require('./lib/parseSchema');
 const {generateData} = require('./lib/genData');
+const {generateSql} = require('./lib/genSql');
 const maxNumber = 50;
 
 
 const sql = fs.readFileSync('./db.sql', 'utf8');
 const json = parser.toCompactJson(parser.feed(sql).results);
 
+
 const parsedSchema = parseSchema(json);
 const jsonData = generateData(parsedSchema, maxNumber);
 
-const output = jsonData;
-fs.writeFileSync('output/schema.json', JSON.stringify(output));
+const queries = generateSql(jsonData);
+
+fs.writeFileSync('output/schema.json', JSON.stringify(jsonData));
+fs.writeFileSync('output/queries.sql', queries);
