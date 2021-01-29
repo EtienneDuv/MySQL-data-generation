d08ebcc1c75acc49ba93581a9520c632b0b884d8
EtienneDuv <etienne.duverney@viacesi.fr>
2021-01-29 12:08:02 +0100

    [add] generate data, no FK

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const {Parser} = require('sql-ddl-to-json-schema');
 const parser = new Parser('mysql');
 
-const {generateData} = require('./lib/genData');
 const {parseSchema} = require('./lib/parseSchema');
+const {generateData} = require('./lib/genData');
 
-const sql = fs.readFileSync('./db.ddl', 'utf8');
+
+const sql = fs.readFileSync('./db.sql', 'utf8');
 const json = parser.toCompactJson(parser.feed(sql).results);
 
-parseSchema(json);
+const parsedSchema = parseSchema(json);
+generateData(parsedSchema, 50);
 
 const output = json;
 fs.writeFileSync('output/schema.json', JSON.stringify(output));
