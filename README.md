d106c06d27e7c685d0733274bd2c6b7c4e4f001c
EtienneDuv <etienne.duverney@viacesi.fr>
2021-02-01 10:35:19 +0100

    details

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # MySQL-data-generation
 Generates data from DDL file
 
-DDL => JSON => Faker generate => Sequelize ?
+`DDL => JSON => Faker generate => SQL`
+
+- install project `npm i`
+- Add database generation `.ddl` file in project root
+- run `npm start`
+- enjoy the data
--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const fs = require('fs');
 const {Parser} = require('sql-ddl-to-json-schema');
 const parser = new Parser('mysql');
 
-const faker = require('faker');
-faker.seed(1);
-
 const {parseSchema} = require('./lib/parseSchema');
 const {generateData} = require('./lib/genData');
 const {generateSql} = require('./lib/genSql');
 const maxNumber = 50;
 
+const sql = fs.readFileSync('./db.dll', 'utf8');
 
-const sql = fs.readFileSync('./db.sql', 'utf8');
 const json = parser.toCompactJson(parser.feed(sql).results);
 
-
 const parsedSchema = parseSchema(json);
+
 const jsonData = generateData(parsedSchema, maxNumber);
 
-const queries = generateSql(jsonData);
+const generatedSql = generateSql(jsonData);
 
+const dir = './output/';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 fs.writeFileSync('output/schema.json', JSON.stringify(jsonData));
-fs.writeFileSync('output/queries.sql', queries);
+fs.writeFileSync('output/queries.sql', generatedSql);
